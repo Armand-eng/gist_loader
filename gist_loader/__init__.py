5e199a9483cc02cf6433ae86def31fcabdbb36b1
takosuke <takosuke@localhost>
2016-06-11 14:30:10 +0900

    commit

--- gist_loader/__init__.py
+++ gist_loader/__init__.py
@@ -1,39 +1,88 @@
 
 bl_info = {
-	"name": "Gist Loader",
-	"author": "Takosuke",
-	"version": (0, 1, 0),
-	"blender": (2, 77, 0),
-	"location": "Text Editor > Properties Panel",
-	"description": "Load gist info and raw data.",
-	"support": "COMMUNITY",
-	"wiki_url": "",
-	"tracker_url": "",
-	"category": "Text"}
+    "name": "Gist Loader",
+    "author": "Takosuke",
+    "version": (0, 2, 0),
+    "blender": (2, 77, 0),
+    "location": "Text Editor > Properties Panel",
+    "description": "Load gist info and raw data.",
+    "support": "COMMUNITY",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Text"}
 
 if "bpy" in locals():
-	import imp
-	imp.reload(gist_loader)
+    import imp
+    imp.reload(gist_loader)
 else:
-	from . import gist_loader
+    from . import gist_loader
 
 import bpy
-from bpy.props import StringProperty, CollectionProperty, IntProperty
+from bpy.props import StringProperty, CollectionProperty, IntProperty, BoolProperty, EnumProperty
 
+class GistLoaderAddonPreferences(bpy.types.AddonPreferences):
+    bl_idname = __name__
+
+    proxy = BoolProperty(name="Proxy", default=False)
+    max_page = IntProperty(name="Max number of pages", description="Max number of pages", default=10, min=10, max=100)
+
+    def draw(self, context):
+        layout = self.layout
+        layout.prop(self, "proxy")
+        layout.prop(self, "max_page")
+
+class GistLoaderSettings(bpy.types.PropertyGroup):
+
+    def start_page_update_event(self, context):
+        settings = context.scene.gist_loader_settings
+        if settings.start_page > settings.end_page:
+            settings.end_page = settings.start_page
+
+    def end_page_update_event(self, context):
+        settings = context.scene.gist_loader_settings
+        if settings.start_page > settings.end_page:
+            settings.start_page = settings.end_page
+
+    addon_name = StringProperty(name="Addon Name", description="Addon Name", default=__name__)
+    user_id = StringProperty(name="User ID", description="Github Gist User ID")
+    gists = CollectionProperty(type=gist_loader.GistInfo)
+    start_page = IntProperty(name="Start", description="Start Page. 30 gists in a page.", default=1, min=1, max=100, update=start_page_update_event)
+    end_page = IntProperty(name="End", description="End Page. 30 gists in a page.", default=1, min=1, max=100, update=end_page_update_event)
+    active_gist_index = IntProperty(name="Active Gist Index", description="active gist index")
+    use_proxy = BoolProperty(name="Use Proxy", description="Use Proxy", default=True)
+    proxy_protocol = StringProperty(name="Protocol", description="Protocol", default="http")
+    proxy_address = StringProperty(name="Address", description="Address")
+    proxy_port = StringProperty(name="Port", description="Port", default="8080")
+
+translations = {
+    "ja_JP": {
+        ("*", "User ID"): "ユーザーID",
+        ("*", "Page:"): "ページ:",
+        ("Operator", "Get Gists Info"): "Gist 情報を取得",
+        ("*", "Description: "): "詳細:",
+        ("Operator", "Load Selected Gists"): "選択したファイルを読み込む",
+        ("*", "Use Proxy"): "プロキシを使う",
+        ("*", "Protocol"): "プロトコル",
+        ("*", "Address"): "アドレス",
+        ("*", "Port"): "ポート",
+        ("*", "Please fill in User ID."): "ユーザーIDを入力して下さい。",
+        ("*", "Please set a range of pages in max number of pages ({0})."): "最大ページ数({0})の範囲内で設定して下さい。",
+        ("*", "Can not found User."): "ユーザーが見つかりません。",
+        ("*", "This User has no gists."): "指定したユーザーにはGistが存在しません。",
+        ("*", "The last page of this user is {0}."): "指定したユーザーの最終ページは{0}です。",
+        ("*", "No items selected."): "アイテムが選択されていません。",
+    }
+}
 
 def register():
-	gist_loader.register()
-
-	bpy.types.Scene.gist_uid_property = StringProperty(name = "Gist UID", description = "Github Gist User ID")
-	bpy.types.Scene.gists = CollectionProperty(type = bpy.types.GistInfo)
-	bpy.types.Scene.active_gist_index = IntProperty(name = "active_gist_index", description = "active_gist_index")
+    bpy.utils.register_module(__name__)
+    bpy.types.Scene.gist_loader_settings = bpy.props.PointerProperty(type=GistLoaderSettings)
+    bpy.app.translations.register(__name__, translations)
 
 def unregister():
-	del bpy.types.Scene.gist_uid_property
-	del bpy.types.Scene.gists
-	del bpy.types.Scene.active_gist_index
-
-	gist_loader.unregister()
+    bpy.app.translations.unregister(__name__)
+    del bpy.types.Scene.gist_loader_settings
+    bpy.utils.unregister_module(__name__)
 
 if __name__ == "__main__":
-	register()
+    register()
--- gist_loader/gist_loader.py
+++ gist_loader/gist_loader.py
@@ -1,190 +1,284 @@
 import bpy
 import collections
+import datetime
 import json
 import os
+import requests
+import time
+import urllib
 from bpy.props import StringProperty, IntProperty, BoolProperty
-from urllib.request import urlopen
+from bpy.app.translations import pgettext, pgettext_iface
 
 class GetGistsInfoButton(bpy.types.Operator):
-	bl_idname = "scene.get_gists_info"
-	bl_label = "Load gists data from user id."
+    bl_idname = "scene.get_gists_info"
+    bl_label = "Load gists data from user id."
 
-	key_files = "files"
-	key_desc = "description"
-	key_filename = "filename"
-	key_raw_url = "raw_url"
+    key_header_limit = "X-RateLimit-Limit"
+    key_header_remaining = "X-RateLimit-Remaining"
+    key_header_reset = "X-RateLimit-Reset"
+    key_files = "files"
+    key_desc = "description"
+    key_filename = "filename"
+    key_raw_url = "raw_url"
+    key_url = "url"
+    key_link = "link"
+    key_last = "last"
+    key_page = "page"
 
-	def execute(self, context):
-		user_id = context.scene.gist_uid_property
+    url_param_page = "page"
 
-		if user_id:
-			context.scene.active_gist_index = 0
-			self.get_gists(context, user_id)
-		else:
-			bpy.ops.error.gist_loader('INVOKE_DEFAULT',message = "Please fill in User ID.")
+    def execute(self, context):
+        print("----- get gists info start " + datetime.datetime.now().strftime("%H:%M:%S") + " -----")
 
-		return{'FINISHED'}
+        requests.packages.urllib3.disable_warnings()
+        user_id = context.scene.gist_loader_settings.user_id
 
-	def get_gists(self, context, user_id):
-		url = "https://api.github.com/users/" + user_id + "/gists"
-		open_url = None
-		try:
-			open_url = urlopen(url)
-			if not open_url: return
-		except Exception as ex:
-			bpy.ops.error.gist_loader('INVOKE_DEFAULT',message = "Can not found User.")
-			return
+        if user_id:
+            context.scene.gist_loader_settings.active_gist_index = 0
+            self.get_gists(context, user_id)
+        else:
+            bpy.ops.error.gist_loader('INVOKE_DEFAULT',message=pgettext_iface("Please fill in User ID."))
 
-		json_raw = open_url.read().decode('UTF-8')
-		gists_data = json.loads(json_raw, object_pairs_hook=collections.OrderedDict)
+        print("----- get gists info end   " + datetime.datetime.now().strftime("%H:%M:%S") + " -----")
 
-		scn = context.scene
-		for i in range(0, len(scn.gists)):
-			scn.gists.remove(0)
+        return{'FINISHED'}
 
-		for i, gist_data in enumerate(gists_data):
-			for file_data in gist_data[self.key_files].values():
-				gist = scn.gists.add()
-				gist.file_name = file_data[self.key_filename]
-				gist.raw_url = file_data[self.key_raw_url]
-				gist.desc = gist_data[self.key_desc]
+    def get_gists(self, context, user_id):
+        settings = context.scene.gist_loader_settings
+        max_page = context.user_preferences.addons[settings.addon_name].preferences.max_page
+        url = "https://api.github.com/users/" + user_id + "/gists"
+
+        if settings.end_page - settings.start_page + 1 > max_page:
+            bpy.ops.error.gist_loader('INVOKE_DEFAULT', message=pgettext_iface("Please set a range of pages in max number of pages ({0}).").format(max_page))
+            return
+
+        proxies = self.get_proxies(context, settings)
+
+        start_page = settings.start_page
+
+        # 最初のページだけ取得し、ヘッダを調査
+        params_start = {self.url_param_page: str(start_page)}
+        request_start = requests.get(url, verify=False, params=params_start, proxies=proxies)
+        if not request_start:
+            bpy.ops.error.gist_loader('INVOKE_DEFAULT', message=pgettext_iface("Can not found User."))
+            return
+
+        print("page number : {0}".format(start_page))
+        self.print_limit(request_start)
+
+        last_page = 1
+        if self.key_link in request_start.headers:
+            if self.key_last in request_start.links:
+                last_url = request_start.links[self.key_last][self.key_url]
+                splited_url = urllib.parse.urlsplit(last_url)
+                query_raw = urllib.parse.parse_qs(splited_url.query)
+                last_page = int(dict(query_raw)[self.key_page][0])
+            else:
+                # lastキーが無い場合、そのページが最終ページ
+                last_page = start_page
+
+        print("last page : {0}".format(last_page))
+
+        json_start = request_start.json()
+        if not json_start:
+            if start_page == 1:
+                bpy.ops.error.gist_loader('INVOKE_DEFAULT', message=pgettext_iface("This User has no gists."))
+                return
+            elif last_page < settings.start_page:
+                bpy.ops.error.gist_loader('INVOKE_DEFAULT', message=pgettext_iface("The last page of this user is {0}.").format(last_page))
+                settings.start_page = last_page
+                settings.end_page = last_page
+                return
+
+        if last_page < settings.end_page:
+            bpy.ops.error.gist_loader('INVOKE_DEFAULT', type="Message", message=pgettext_iface("The last page of this user is {0}.").format(last_page))
+            settings.end_page = last_page
+
+        end_page = settings.end_page
+
+        gists_data_start = request_start.json()
+
+        for i in range(0, len(settings.gists)):
+            settings.gists.remove(0)
+
+        self.add_gist(settings.gists, gists_data_start)
+
+        # 以降のページを取得
+        for page_num in range(start_page + 1, end_page + 1):
+            params = {self.url_param_page: str(start_page)}
+            request = requests.get(url, params=params, verify=False, proxies=proxies)
+
+            print("page number : {0}".format(page_num))
+            self.print_limit(request)
+
+            json = request.json()
+            self.add_gist(settings.gists, json)
+
+    def get_proxies(self, context, settings):
+        proxies = {}
+
+        if context.user_preferences.addons[settings.addon_name].preferences.proxy and settings.use_proxy:
+            protocol = settings.proxy_protocol
+            address = settings.proxy_address
+            port = settings.proxy_port
+            proxies = { protocol: "http://" + address + ":" + port}
+
+        return proxies
+
+    def print_limit(self, request):
+        limit = request.headers[self.key_header_limit]
+        remaining = request.headers[self.key_header_remaining]
+        reset = request.headers[self.key_header_reset]
+
+        print(self.key_header_limit + " : " + limit)
+        print(self.key_header_remaining + " : " + remaining)
+
+        time_raw = time.localtime(int(reset))
+        time_str = time.strftime("%Y/%m/%d %H:%M:%S", time_raw)
+        print(self.key_header_reset + " : " + time_str)
+
+    def add_gist(self, gists, gists_data):
+        for i, gist_data in enumerate(gists_data):
+            for file_data in gist_data[self.key_files].values():
+                gist = gists.add()
+                gist.file_name = file_data[self.key_filename]
+                gist.raw_url = file_data[self.key_raw_url]
+                desc = gist_data[self.key_desc]
+                if desc != None:
+                    gist.desc = gist_data[self.key_desc]
 
 class LoadGistsTextButton(bpy.types.Operator):
-	bl_idname = "scene.load_gists"
-	bl_label = "Load checked gists."
+    bl_idname = "scene.load_gists"
+    bl_label = "Load checked gists."
 
-	def execute(self, context):
-		self.create_text(context)
+    def execute(self, context):
+        print("----- get gists text start " + datetime.datetime.now().strftime("%H:%M:%S") + " -----")
+        self.create_text(context)
+        print("----- get gists text end   " + datetime.datetime.now().strftime("%H:%M:%S") + " -----")
 
-		return{'FINISHED'}
+        return{'FINISHED'}
 
-	def create_text(self, context):
-		gists = context.scene.gists
-		selected_gists = [gist for gist in gists if gist.toggle_load_file]
+    def create_text(self, context):
+        gists = context.scene.gist_loader_settings.gists
+        selected_gists = [gist for gist in gists if gist.toggle_load_file]
 
-		if len(selected_gists) <= 0:
-			bpy.ops.error.gist_loader('INVOKE_DEFAULT',message = "No items selected.")
-			return
+        if len(selected_gists) <= 0:
+            bpy.ops.error.gist_loader('INVOKE_DEFAULT',message=pgettext_iface("No items selected."))
+            return
 
-		for gist in selected_gists:
+        for gist in selected_gists:
+            file_name = gist.file_name
+            raw_url = gist.raw_url
+            print(file_name + " : " + raw_url)
 
-			file_name = gist.file_name
-			raw_url = gist.raw_url
-
-			raw = urlopen(raw_url).read().decode('UTF-8')
-			new_text = bpy.data.texts.new(file_name)
-			new_text.write(raw)
+            request = requests.get(raw_url, verify=False)
+            # print(request.headers)
+            text = request.text
+            new_text = bpy.data.texts.new(file_name)
+            new_text.write(text)
 
 class GistInfo(bpy.types.PropertyGroup):
-	toggle_load_file = BoolProperty(default=False)
-	file_name = StringProperty()
-	raw_url = StringProperty()
-	desc = StringProperty()
+    toggle_load_file = BoolProperty(default=False)
+    file_name = StringProperty()
+    raw_url = StringProperty()
+    desc = StringProperty()
 
 class TEXT_UL_gistslots(bpy.types.UIList):
-	text_exts = [".txt", ".yml", ".ini", ".md"]
-	script_exts = [".py"]
+    text_exts = [".txt", ".yml", ".ini", ".md"]
+    script_exts = [".py", ".osl"]
 
-	def draw_item(self, context, layout, data, item, icon, active_data, active_propname):
-		slot = item
-		if slot:
-			path, ext = os.path.splitext(slot.file_name)
-			icon_file_name = 'FILE'
-			if ext in self.text_exts:
-				icon_file_name = 'FILE_TEXT'
-			elif ext in self.script_exts:
-				icon_file_name = 'FILE_SCRIPT'
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname):
+        slot = item
+        if slot:
+            path, ext = os.path.splitext(slot.file_name)
+            icon_file_name = 'FILE'
+            if ext in self.text_exts:
+                icon_file_name = 'FILE_TEXT'
+            elif ext in self.script_exts:
+                icon_file_name = 'FILE_SCRIPT'
 
-			col_name = layout.column()
-			col_name.label(text=slot.file_name, icon=icon_file_name)
+            col_name = layout.column()
+            col_name.label(text=slot.file_name, translate=False, icon=icon_file_name)
 
-			col_load = layout.column()
-			col_load.alignment = 'RIGHT'
-			col_load.prop(slot, "toggle_load_file", icon_only=True)
-		else:
-			layout.label(text="", translate=False, icon_value=icon)
+            col_load = layout.column()
+            col_load.alignment = 'RIGHT'
+            col_load.prop(slot, "toggle_load_file", icon_only=True)
+        else:
+            layout.label(text="", translate=False, icon_value=icon)
 
-	def filter_items(self, context, data, propname):
-		gists = getattr(data, propname)
-		helper_funcs = bpy.types.UI_UL_list
-		flt_flags = []
-		flt_neworder = []
+    def filter_items(self, context, data, propname):
+        gists = getattr(data, propname)
+        helper_funcs = bpy.types.UI_UL_list
+        flt_flags = []
+        flt_neworder = []
 
-		flt_flags = [self.bitflag_filter_item] * len(gists)
-		# print(self.filter_name)
+        flt_flags = [self.bitflag_filter_item] * len(gists)
 
-		for idx, gist in enumerate(gists):
-			if self.filter_name not in gist.file_name and self.filter_name not in gist.desc:
-				flt_flags[idx] &= ~self.bitflag_filter_item
+        for idx, gist in enumerate(gists):
+            if self.filter_name not in gist.file_name and self.filter_name not in gist.desc:
+                flt_flags[idx] &= ~self.bitflag_filter_item
 
-		if self.use_filter_sort_alpha:
-			flt_neworder = helper_funcs.sort_items_by_name(gists, "file_name")
+        if self.use_filter_sort_alpha:
+            flt_neworder = helper_funcs.sort_items_by_name(gists, "file_name")
 
-		return flt_flags, flt_neworder
+        return flt_flags, flt_neworder
 
 class TEXT_PT_gist_load(bpy.types.Panel):
-	bl_idname = "TEXT_PT_gist_load"
-	bl_label = "Gist Loader"
-	bl_space_type = "TEXT_EDITOR"
-	bl_region_type = "UI"
-	bl_context = "object"
+    bl_idname = "TEXT_PT_gist_load"
+    bl_label = "Gist Loader"
+    bl_space_type = "TEXT_EDITOR"
+    bl_region_type = "UI"
+    bl_context = "object"
 
-	def draw(self, context):
-		layout = self.layout
-		row = layout.row()
-		scn = bpy.context.scene
-		obj = context.object
+    def draw(self, context):
+        layout = self.layout
+        settings = bpy.context.scene.gist_loader_settings
+        use_translate_interface = context.user_preferences.system.use_translate_interface
 
-		layout.prop(scn, "gist_uid_property")
-		layout.operator("scene.get_gists_info", text='Get Gists Info')
-		layout.template_list("TEXT_UL_gistslots", "", scn, "gists", scn, "active_gist_index", rows= 8)
-		layout.label("Description")
+        layout.prop(settings, "user_id")
+        row = layout.row()
+        row.label(pgettext_iface("Page:"))
+        row.prop(settings, "start_page")
+        row.prop(settings, "end_page")
 
-		desc = ""
-		if len(scn.gists) > 0:
-			active_gist = scn.gists[scn.active_gist_index]
-			desc = active_gist.desc
+        if context.user_preferences.addons[settings.addon_name].preferences.proxy:
+            layout.prop(settings, "use_proxy")
+            layout.prop(settings, "proxy_protocol")
+            layout.prop(settings, "proxy_address")
+            layout.prop(settings, "proxy_port")
 
-		box = layout.box()
-		box.label(desc)
+        layout.operator("scene.get_gists_info", text=pgettext("Get Gists Info"))
+        layout.template_list("TEXT_UL_gistslots", "", settings, "gists", settings, "active_gist_index", rows= 8)
+        layout.label(pgettext_iface("Description: "))
 
-		layout.operator("scene.load_gists", text='Load Selected Gists')
+        desc = ""
+        if len(settings.gists) > 0:
+            active_gist = settings.gists[settings.active_gist_index]
+            desc = active_gist.desc
+
+        box = layout.box()
+        box.label(desc)
+
+        layout.operator("scene.load_gists", text=pgettext("Load Selected Gists"))
 
 class GistLoaderMessageOperator(bpy.types.Operator):
-	bl_idname = "error.gist_loader"
-	bl_label = "Message"
-	type = StringProperty(default="Error")
-	message = StringProperty()
+    bl_idname = "error.gist_loader"
+    bl_label = "Message"
+    type = StringProperty(default="Error")
+    message = StringProperty()
 
-	def execute(self, context):
-		self.report({'INFO'}, self.message)
-		print(self.message)
-		return {'FINISHED'}
+    def execute(self, context):
+        self.report({'INFO'}, self.message)
+        print(self.message)
+        return {'FINISHED'}
 
-	def invoke(self, context, event):
-		wm = context.window_manager
-		return wm.invoke_popup(self, width=300, height=200)
+    def invoke(self, context, event):
+        wm = context.window_manager
+        return wm.invoke_popup(self, width=300, height=200)
 
-	def draw(self, context):
-		self.layout.label(text="", icon='ERROR')
-		self.layout.label(text=self.message)
-
-classes = [
-	TEXT_UL_gistslots,
-	TEXT_PT_gist_load,
-	GetGistsInfoButton,
-	LoadGistsTextButton,
-	GistInfo,
-	GistLoaderMessageOperator
-]
-
-def register():
-	for cls in classes:
-		bpy.utils.register_class(cls)
-
-
-def unregister():
-	for cls in classes:
-		bpy.utils.unregister_class(cls)
-
-
+    def draw(self, context):
+        if self.type == "Message":
+            self.layout.label(text="", icon='INFO')
+        else:
+            self.layout.label(text="", icon='ERROR')
+        self.layout.label(text=self.message)
